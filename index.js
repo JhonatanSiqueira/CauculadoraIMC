a3063b532b9e754a1fe0efd9bfd8d6172005a0ef
Jhonatan Siqueira Pereira <79463243+JhonatanSiqueira@users.noreply.github.com>
2021-10-08 14:03:23 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+//Declaraçãod e variáveis alteráveis.
+const peso = 70;
+const altura = 1.82;
+
+//Variável constante.
+const imc = peso / (altura * altura);
+
+//Imprimi na tela!
+console.log('--------------------------------')
+console.log("Seu IMC é de:",imc);
+console.log('--------------------------------')
+
+if(imc <= 18)
+{
+  "Você está abaixo do peso, se cuide!"
+}
+else if(18 < imc >= 25)
+{
+  "Seu peso está normal, maravilha!"
+}
+else if( 25 < imc >= 29)
+{
+  "Você apresenta sobrepeso. Atenção!"
+}
+else if( 30 < imc >= 35)
+{
+  "Obesidade (1). Consulte um médico!"
+}
+else if( 35 < imc >= 40 )
+{
+  "Obesidade severa. Precisa de cuidados urgente!"
+}
+else if(imc >= 40)
+{
+  "Obesidade mórbida! Crie novos hábitos e consulte um ´rofissional, urgente!"
+}
+else {"imc incorreto"};
+
